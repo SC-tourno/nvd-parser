c60af66a65abac6424847e9afb9b951049676f1e
SC <alexsuryapranata@utexas.edu>
2017-07-12 09:56:14 -0400

    added more error-checking; allows more input files

--- extractCVE.sh
+++ extractCVE.sh
@@ -1,47 +1,69 @@
 #!/bin/sh
 
-# Assume $1 is input csv file
-# $2 is output file
-# Check if xml files are present. If not wget them from nvd website
-filtered_lines=( $(grep "CVE" $1 | cut -d ',' -f 1,3 | sed s/\'//g) )   
-CVE=( $(grep "CVE" $1 | cut -d ',' -f 3 | sed s/\'//g | sort -u) ) > /dev/null
+# Usage: ./extractCVE.sh <input1> <output>
+# can have multiple input files
+# last argument is always output file
 
-if [ -f $2 ]; then
-	rm -f $2
+for lastArg; do :; done # get the last argument of the command line
+echo "Output to $lastArg"
+
+if [ -f $lastArg ]; then
+	rm -f $lastArg
 fi
 
-for CVEID in "${CVE[@]}"
-do
-#	echo $CVEID
-	year=( $(echo $CVEID |cut -d '-' -f 2 ) )
-#	echo $year
-	xmlFileName="nvdcve-2.0-$year.xml"
-	if [ ! -f $xmlFileName ]; then
-		echo "You don't have $xmlFileName!"
-		echo "Let me retrieve that for you..."
-		wget -q "https://static.nvd.nist.gov/feeds/xml/cve/2.0/$xmlFileName.zip"
-		unzip -q "$xmlFileName.zip" && rm -f "$xmlFileName.zip"
+for file; do
+	if [ "$file" == "$lastArg" ]; then
+		break
 	fi
-	startLine=( $(grep -n "$CVEID" $xmlFileName | cut -d ':' -f 1) )
-	range=( $(tail -n +$startLine $xmlFileName | grep -n '</entry>' | cut -d ':' -f 1) )
-	endLine=$((startLine+range-1))
-#	echo $startLine
-#	echo $endLine
+	#filtered_lines=( $(grep "CVE" $file | cut -d ',' -f 1,3 | sed s/\'//g) )   
+	echo "Reading $file"
 
-	ARG1='VENDOR_ADVISORY'
-	ARG2='PATCH'
+	#grep -Po "CVE-\d{4}-\d{4}" $file | cut -d ',' -f 3 | sort -u
+	CVE=( $(grep -Po "CVE-\d{4}-\d{4}" $file | cut -d ',' -f 3 | sort -u) ) > /dev/null
 
-	matchingLine=( $(sed -n -e "$startLine,$endLine s/$ARG1/&/p" -e "$startLine,$endLine s/$ARG2/&/p" $xmlFileName) )
-#	echo "$matchingLine"
 
-	found='False'
-	if [ ! -z "$matchingLine" ]; then
-		found='True' # matching line is NOT empty, ergo a match was found
+	if [ -z CVE ]; then
+		echo "No CVE lines matched in $file"
+		continue       
 	fi
 
-	echo "$CVEID,$found" >> "$2"
-	echo "Outputted to $2"
+	for CVEID in "${CVE[@]}" 
+	do
+	#	echo $CVEID
+		year=( $(echo $CVEID |cut -d '-' -f 2 ) )
+	#	echo $year
+		xmlFileName="nvdcve-2.0-$year.xml"
+		if [ ! -f $xmlFileName ]; then
+			echo "You don't have $xmlFileName!"
+			echo "Let me retrieve that for you..."
+			wget  -q --show-progress "https://static.nvd.nist.gov/feeds/xml/cve/2.0/$xmlFileName.zip"
+			unzip -q "$xmlFileName.zip" && rm -f "$xmlFileName.zip"
+			if [ ! -f $xmlFileName ]; then
+				# if file still does not exist, skip
+				continue
+			fi
+		fi
+		startLine=( $(grep -n "$CVEID" $xmlFileName | cut -d ':' -f 1) )
+		range=( $(tail -n +$startLine $xmlFileName | grep -n '</entry>' | cut -d ':' -f 1) )
+		endLine=$((startLine+range-1))
+	#	echo $startLine
+	#	echo $endLine
+
+		ARG1='VENDOR_ADVISORY'
+		ARG2='PATCH'
+
+		matchingLine=( $(sed -n -e "$startLine,$endLine s/$ARG1/&/p" -e "$startLine,$endLine s/$ARG2/&/p" $xmlFileName) )
+	#	echo "$matchingLine"
+
+		found='False'
+		if [ ! -z "$matchingLine" ]; then
+			found='True' # matching line is NOT empty, ergo a match was found
+		fi
+
+		echo "$CVEID,$found" >> "$lastArg"
+
+	done
 
 done
 
-
+echo "Done!"
