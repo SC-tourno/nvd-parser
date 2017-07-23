30a7b13ed153a8284d2175fccedbf4ea723c03a0
SC <alexsuryapranata@utexas.edu>
2017-07-23 15:48:42 -0400

    changed output file

--- extractCVE.sh
+++ extractCVE.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Usage: ./extractCVE.sh <input1> <output>
+# Usage: ./extractCVE.sh <input1 [input2 ...]>  
 # can have multiple input files
 # last argument is always output file
 
-for output; do :; done # get the last argument of the command line. Last argument = output
+#if [ -z $2 ]; then
+#	output=$1.out
+#else
+#	for output; do :; done # get the last argument of the command line. Last argument = output
+#fi
+if [ -z $1 ]; then
+	echo "Usage: ./extractCVE.sh <input1 [input2 ...]>"
+fi
+
+echo "Type the name of the output file [default: PATCH_$1]:"
+read output
+if [ -z $output ]; then
+	output="PATCH_$1"
+fi
+
 echo "Output to $output"
 
 if [ -f $output ]; then
@@ -50,10 +64,12 @@ for file; do
 		range=( $(tail -n +$startLine $xmlFileName | grep -n '</entry>' | cut -d ':' -f 1) )
 		endLine=$((startLine+range-1))
 
-		ARG1='VENDOR_ADVISORY'
+#		Not every vulnerability has a patch (e.g. CVE-2004-2761) Best to err on the safe side and only output true if link with "PATCH" exists
+#		ARG1='VENDOR_ADVISORY'
 		ARG2='PATCH'
 
-		matchingLine=( $(sed -n -e "$startLine,$endLine s/$ARG1/&/p" -e "$startLine,$endLine s/$ARG2/&/p" $xmlFileName) )
+#		matchingLine=( $(sed -n -e "$startLine,$endLine s/$ARG1/&/p" -e "$startLine,$endLine s/$ARG2/&/p" $xmlFileName) )
+		matchingLine=( $(sed -n -e "$startLine,$endLine s/$ARG2/&/p" $xmlFileName) )
 
 		found='False'
 		if [ ! -z "$matchingLine" ]; then
